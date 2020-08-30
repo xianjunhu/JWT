bcd92c881ef6b8f489a4f8b3cdd08cf0f202b7c2
Roy Hu <roy.x.hu@gmail.com>
2020-08-30 17:49:07 -0400

    first commit

---
+++ src/main/java/com/example/demo/domain/Vehicle.java
@@ -0,0 +1,26 @@
+package com.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "vehicles")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Vehicle extends AbstractAuditableEntity<User, Long> implements Serializable {
+
+    @Column
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Brand brand = Brand.FORD;
+
+}
